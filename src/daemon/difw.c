4d29870bf5c816b935383d93302aca93e66b909a
John McDowall <john@mcdowall.com>
2024-04-15 16:30:30 -0700

    added main for difw

---
+++ src/daemon/difw.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <config file>\n", argv[0]);
+        return 1;
+    }
+
+    if (daemon(0, 0) == -1)
+    {
+        perror("daemon");
+        return 1;
+    }
+
+    if (difw_init(argv[1]) == -1)
+    {
+        return 1;
+    }
+
+    if (difw_run() == -1)
+    {
+        return 1;
+    }
+
+    return 0;
+}
